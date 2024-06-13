23c973cdb2179faf2b08da1c31ce2e2777cb7c97
facundo-cejas9 <facundo962010@hotmail.com>
2024-06-13 20:49:54 -0300

    last change

--- src/data/data.ts
+++ src/data/data.ts
@@ -73,7 +73,7 @@ export const ProjectsDataEs = [
             {
                 name: "Clon de Netflix",
                 description: "Proyecto personal, clon de Netflix creado con ReactJS para el frontend, firebase para el backend y css puro para los estilos. La aplicación cuenta con un sistema de inicio de sesión y si el ususario no tiene cuenta, se puede crear una.",
-                image: "/images/netflix.png",
+                image: "/images/netflix.webp",
                 link: "https://projectnetfilx.netlify.app/",
                 github: "https://github.com/facundo-cejas9/netflixClone",
             },
@@ -81,7 +81,7 @@ export const ProjectsDataEs = [
             {
                 name: "AlkyBank",
                 description: "Proyecto realizado en la aceleración de Alkemy de ReactJS, la cual consistió en armar un dashboard para una simulación de un banco. Este incluía una página de inicio de sesión, una página de registro y la capacidad de realizar transacciones como enviar y recibir dinero. Se implementaron React, Redux para manejo de estados y CSS Modules.",
-                image: "/images/alkybank.png",
+                image: "/images/alkybank.webp",
                 link: "https://github.com/facundo-cejas9/alkybank",
                 github: "https://github.com/facundo-cejas9/alkybank",
             },
@@ -89,7 +89,7 @@ export const ProjectsDataEs = [
             {
                 name: "Aplicación de notas",
                 description: "Aplicación creada en React con el curso de Fernando Herrera. La aplicación cuenta con funcionalidades de registro y inicio de sesión utilizando Firebase como base de datos. Permite crear notas y recordatorios estilo agenda, además de la capacidad de subir imágenes. Se utilizó React y MaterialUI para los estilos.",
-                image: "/images/journal.png",
+                image: "/images/journal.webp",
                 link: "https://journalv2.netlify.app/",
                 github: "#",
             },
@@ -97,7 +97,7 @@ export const ProjectsDataEs = [
             {
                 name: "Aplicación de comidas",
                 description: "Proyecto Full stack, utilizando MERN. Cuenta con un sistema de inicio de sesión, como así también la posibilidad de que el usuario pueda crearse una cuenta, permite un seguimiento de su pedido, como así ver sus ordenes. La aplicación tiene un panel de admin, el cual permite agregar o eliminar más comidas al backend. Para los métodos de pago utilicé Stripe.",
-                image: "/images/comida.png",
+                image: "/images/comida.webp",
                 link: "https://www.youtube.com/watch?v=vU4XzeodzG0",
                 github: "https://github.com/facundo-cejas9/comidasApp",
             },
@@ -115,7 +115,7 @@ export const ProjectsDataEng = [
             {
                 name: "Netflix Clone",
                 description: "Personal project, Netflix clone created with ReactJS for the frontend, firebase for the backend, and pure css for the styles. The application has a login system and if the user does not have an account, one can be created.",
-                image: "/images/netflix.png",
+                image: "/images/netflix.webp",
                 link: "https://projectnetfilx.netlify.app/",
                 github: "https://github.com/facundo-cejas9/netflixClone",
             },
@@ -123,7 +123,7 @@ export const ProjectsDataEng = [
             {
                 name: "AlkyBank",
                 description: "Project carried out in the Alkemy acceleration of ReactJS, which consisted of putting together a dashboard for a simulation of a bank. This included a login page, a registration page, and the ability to make transactions such as sending and receiving money. React, Redux for state management and CSS Modules were implemented.",
-                image: "/images/alkybank.png",
+                image: "/images/alkybank.webp",
                 link: "https://github.com/facundo-cejas9/alkybank",
                 github: "https://github.com/facundo-cejas9/alkybank",
             },
@@ -131,7 +131,7 @@ export const ProjectsDataEng = [
             {
                 name: "Journal APP",
                 description: "Application created in React with the course of Fernando Herrera. The app has registration and sign-in capabilities using Firebase as a database. It allows you to create agenda-style notes and reminders, as well as the ability to upload images. React and MaterialUI were used for the styles.",
-                image: "/images/journal.png",
+                image: "/images/journal.webp",
                 link: "https://journalv2.netlify.app/",
                 github: "#",
             },
@@ -139,7 +139,7 @@ export const ProjectsDataEng = [
             {
                 name: "Food Aplication",
                 description: "Full stack project, using MERN. It has a login system, as well as the possibility for the user to create an account, it allows you to track your order, as well as see your orders. The app has an admin panel, which allows you to add or remove more meals to the backend. For payment methods I used Stripe.",
-                image: "/images/comida.png",
+                image: "/images/comida.webp",
                 link: "https://www.youtube.com/watch?v=vU4XzeodzG0",
                 github: "https://github.com/facundo-cejas9/comidasApp",
             },
